c5f6ebd5a348ce4f0b3817e12b907425d3ce171a
Juan Acevedo <juan.acevedo9805@gmail.com>
2025-07-20 00:40:24 -0500

    feat: Add SSL for localhost

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: 'taskpro.local',
+    port: 5173,
+    strictPort: true,         
+    allowedHosts: ['taskpro.local'],
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'certs/localhost.crt'))
+    }
+  },
   plugins: [
     react(),
     tailwindcss()
